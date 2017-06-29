9559d0eaaf9a4a865521b42b4cad942f07d9ad7c
cadaverousblue <changbo89757@sina.com>
2017-06-29 17:28:30 +0800

    #修复#tabbar 显示问题

--- Chat/ChatListviewcontroller.swift
+++ Chat/ChatListviewcontroller.swift
@@ -23,7 +23,7 @@ class ChatListviewcontroller: RCConversationListViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        delay(1) {
+        delay(0) {
             self.tabBarController?.hidesBottomBarWhenPushed = false
         }
     }
